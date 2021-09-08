884f594148748007d2575014e863a9443a7e0512
Etienne-COMBE <etiennecombe@gmail.com>
2021-09-08 13:56:15 +0200

    Adding cleaning part

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def check_unique(series: pd.Series):
+    if series.nunique() != series.count():
+        print(f"{series.name} is not unique")
+    return
+
+def check_positive(series: pd.Series):
+    if series.min() < 0:
+        print(f"{series.name} has negative values")
+    return
+
+def check_duplicates_without_id(df: pd.DataFrame, index: str):
+    duplicates = df[df[df.columns.drop('car_ID')].duplicated()]
+    if len(duplicates) > 0:
+        print(duplicates)
+    return
