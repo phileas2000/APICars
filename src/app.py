fa603aa9321d322a766ecd12535ae757dac5c5e4
Etienne-COMBE <etiennecombe@gmail.com>
2021-09-15 11:01:55 +0200

    Configuring for Heroku deployement

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ lin_reg = pickle.load(open('../model/linear_reg_model.sav', 'rb'))
 app = FastAPI()
 
 @app.get('/predict/')
-def root(curbweight: int, enginesize: int):
+def predict_price(curbweight: int, enginesize: int, ):
     X = pd.DataFrame({'curbweight': [curbweight], 'enginesize': [enginesize]})
     y_pred = lin_reg.predict(X)
     return {'price': y_pred[0][0]}
